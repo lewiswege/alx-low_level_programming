48770124c013c6e1f324d8c5650d99d9da7c0093
lewiswege <wegelewis7@gmail.com>
2023-08-16 17:38:42 +0300

    fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - prints suma of all even fibonacci numbers under 4000000
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	unsigned long x, y, z, sum;
+
+	z = 0;
+	x = 0;
+	y = 1;
+	sum = 0;
+
+	while (z < 4000000)
+	{
+		z = x + y;
+		x = y;
+		y = z;
+		if (z % 2 == 0)
+			sum += z;
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
