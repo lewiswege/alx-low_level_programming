bb52481f68c85354ac43f621fe4288711d42be6c
lewiswege <wegelewis7@gmail.com>
2023-08-22 10:57:49 +0300

    upper case and lowercaase

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -6,10 +6,10 @@
  *
  * Return: 0 or 1
  */
+int _isupper(int c)
 
-int _isupper(int c);
-
-{	if (c >= 'A' && c <= 'Z')
+{
+	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,3 +3,4 @@
 
 
 int _isupper(int c);
+#endif 
