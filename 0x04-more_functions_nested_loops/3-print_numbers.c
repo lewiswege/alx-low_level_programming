c965b6849fac5e1dada77fb7d63910e434d7d584
lewiswege <wegelewis7@gmail.com>
2023-08-22 12:03:06 +0300

    changed few lines in the script

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,18 @@
 #include "main.h"
+
 /**
- * print_numbers - prints numbers from 0 to 9 followed by new line
- *
- * Return: 0 (Success)
- */
+*print_numbers - print 0123456789
+* owned by Bwave ICT
+*Return: void
+*/
+
 void print_numbers(void)
 {
-	int i;
+	char c;
 
-	for (i = 0; i <= 9; i++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		_putchar(i + '0');
+	_putchar(c);
 	}
 	_putchar('\n');
 }
