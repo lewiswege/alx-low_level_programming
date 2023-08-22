aec6923f4b52979fe7e9f686c29445f293324347
lewiswege <wegelewis7@gmail.com>
2023-08-22 19:38:38 +0300

    returns the lengyh of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,23 @@
+/**
+ * _strlen - Returns the length of a string.
+ * @s: Pointer to the input string.
+ *
+ * Description:
+ * This function takes a pointer to a null-terminated string as a parameter
+ * and returns the length of the string (excluding the null terminator).
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *s)
+{
+	 int length = 0;
+
+	 while (*s != '\0') 
+{
+	length++;
+	s++;
+}
+
+    return length;
+}
+
