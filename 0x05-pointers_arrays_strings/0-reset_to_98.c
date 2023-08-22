fae45f5db480c614848d2d587d394a7977260564
lewiswege <wegelewis7@gmail.com>
2023-08-22 19:17:08 +0300

    a pointer that updates value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+*takes a parameter to an int and updates the value to 98
+*return: 0 is success
+*/
+void reset_to_98(int *n){
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+The directory contains ALX projects on arrays pointers strings and memory layout- journey with me
